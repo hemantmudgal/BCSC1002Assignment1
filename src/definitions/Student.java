f77cfd605ea8f055b6f74a4f6e2743c12f222484
Hemant Mudgal <hemant.mudgal_cs19@gla.ac.in>
2020-08-30 12:09:49 +0530

    Add fields for Student.java class.

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -7,4 +7,12 @@
 package definitions;
 
 public class Student {
+    private static int initialNumberofBookIssuedByStudent = 0;
+    private final int LIMIT_ON_ISSUING_BOOK_FORSTUDENT = 3;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private long rollNumber;
+    private int totalBookSIssuedByStudent;
+    private Book[] BooksIssuedByStudent;
 }
