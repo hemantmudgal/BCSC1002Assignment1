3c95fb2596a703064d0e9a3a8363d1b451d7eee8
Hemant Mudgal <hemant.mudgal_cs19@gla.ac.in>
2020-08-30 15:06:02 +0530

    Add fields for FrontDesk.java class.

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -7,6 +7,16 @@
 package execution;
 
 public class FrontDesk {
+    private static final int ADD_NEW_BOOK = 1;
+    private static final int RETURN_BOOK = 2;
+    private static final int MY_ISSUES_BOOK = 3;
+    private static final int EXIT = 4;
+    private static final int SHOW_BOOKS = 3;
+    private static final int INSERT_ISBN = 0;
+    private static final int STUDENT = 2;
+    private static final int LIBRARIAN = 1;
+    private static final int REMOVE_BOOK = 2;
+
     public static void main(String[] args) {
 
     }
